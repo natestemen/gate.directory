180f481d8f1f3e6e1fd639677cfca175b99b766c
nate stemen <nate@unitary.foundation>
2025-01-19 14:03:44 -0800

    styling + fixing bugs
    co-authored-by: Rafael De Los Santos <rdelos747@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("styles/base.css");
+
   eleventyConfig.addCollection("gates", function (collectionApi) {
     return collectionApi.getFilteredByGlob("gates/*.md");
+  });
+
+  eleventyConfig.addCollection("groups", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("groups/*.md");
   });
 
   return {
